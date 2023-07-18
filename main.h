7afbf679feb662ac6a7ab8460358c5ead024c5ee
JoseMilans <josemilans@gmail.com>
2023-07-17 20:09:16 -0700

    main.h Added new func prototypes. Updated the struct elements.

--- main.h
+++ main.h
@@ -3,25 +3,28 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * struct specifier_s - struct representing printing functions for specifiers
- * @spec: format specifier
- * @f: func to print a format espec
+ * @spec: format specifier str
+ * @f: fn to print a format espec
  *
  * This struct holds a char, representing the format spec and a ptr to the func
  * that handles the printing of that spec
 */
-typedef struct specifier_s
+typedef struct print
 {
-    char spec;
+    char *p;
     int (*f)(va_list);
-} specifier_t;
+} print_t;
 
+int print_integer(int num);
+int digit_count(int num);
+int abs_val(int num);
+int print_int(va_list args);
 int print_char(va_list args);
 int print_string(va_list args);
-int print_percent(va_list args);
+int (*get_print_func(const char *format))(va_list);
 int _printf(const char *format, ...);
 
 #endif
