a49f955c21ebc8f4134afe1334e4accac201816d
JoseMilans <josemilans@gmail.com>
2023-07-17 20:05:10 -0700

    _printf.c Debug: handle condition when format spec is invalid and when it's the end of the format str ('\0'). get_print_func now selects the appropriate func to handle each format spec.

--- _printf.c
+++ _printf.c
@@ -1,52 +1,6 @@
 #include "main.h"
 /**
- * handle_format - handles the format char
- * @format: ptr to current char in the format str
- * @args: arg list to be processed
- * Return: num of printed chars
- */
-int handle_format(const char *format, va_list args)
-{
-	int i, j, printed_chars = 0;
-	specifier_t specifiers[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{0, NULL}
-	};
-	for (i = 0; format != NULL && format[i] != '\0'; i++)
-	{
-		if (format[i] == '%') /* Format spec */
-		{
-			i++;
-			if (format[i] == '%')
-			{
-				write(1, &format[i], 1);
-				printed_chars++;
-				continue;
-			}
-			else
-			{ /* Find the appropriate func to handle the spec */
-				for (j = 0; specifiers[j].spec != 0; j++)
-				{
-					if (format[i] == specifiers[j].spec)
-					{
-						printed_chars += specifiers[j].f(args);
-						break;
-					}
-				}
-			}
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			printed_chars++;
-		}
-	}
-	return (printed_chars);
-}
-/**
- * _printf - custom printf func
+ * _printf - custom printf fn
  * @format: format str
  * @...: variable arguments passed to the function
  * Return: num of chars printed
@@ -54,12 +8,62 @@ int handle_format(const char *format, va_list args)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int printed_chars;
-    /* Initialise va_list */
-	va_start(args, format);
-    /* Main func to handle format specs */
-	printed_chars = handle_format(format, args);
-    /* Clean up list */
-	va_end(args);
-	return (printed_chars);
+	int (*handler_fn)(va_list);
+	unsigned int i = 0, count = 0;
+
+	if (format == NULL) /* Check if format is NULL */
+		return (-1);
+	va_start(args, format); /* Initialise va_list */
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			i++, count++;
+		}
+		else
+		{
+			handler_fn = get_print_func(&format[i + 1]);
+			if (handler_fn != NULL)
+			{
+				i += 2, count += handler_fn(args);
+			}
+			else if (format[i + 1] != '\0')
+			{
+				write(1, &format[i], 1);
+				i++, count++;
+			}
+			else
+			{
+				return (-1);
+			}
+		}
+	}
+	va_end(args); /* Clean up list */
+	return (count);
+}
+/**
+ * get_print_func - function to get the fn ptr corresponding to a
+ * format spec
+ * @format: Ptr to the start of a format specifier
+ *
+ * Return: function pointer to the corresponding handler function
+ */
+int (*get_print_func(const char *format))(va_list)
+{
+	unsigned int i;
+
+	print_t print_types[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_int},
+		{"i", print_int},
+		{NULL, NULL}
+	};
+	for (i = 0; print_types[i].p != NULL; i++)
+	{
+		if (*(print_types[i].p) == *format)
+			break;
+	}
+	return (print_types[i].f);
 }
